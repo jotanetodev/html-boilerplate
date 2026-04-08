13190be75bcfbb35c26d906aa5e09dd32890a7c9
José Dias Neto <jdneto@live.com>
2026-04-08 19:43:11 -0300

    Remove debug `console.log`

--- scripts/copy-html.js
+++ scripts/copy-html.js
@@ -9,8 +9,6 @@ const publicFilePath = `../public/${fileName}`;
 const source = path.join(__dirname, sourceFilePath);
 const public = path.join(__dirname, publicFilePath);
 
-console.log(source, public)
-
 fileSystem.copyFileSync(source, public);
 
 console.log(`Copied ${fileName} from src to public`);
