4a911afd699c3d4781ce7f256c3d303c5b123d0f
José Dias Neto <jdneto@live.com>
2026-04-09 20:10:42 -0300

    Refactor to use `helper.js` functions

--- scripts/copy-html.js
+++ scripts/copy-html.js
@@ -1,16 +1,9 @@
 // Copy src/index.html to public/index.html.
 
-const fileSystem = require("fs");
-const path = require("path");
+const { publicPathFor, sourcePathFor, copyFile } = require("./helper")
 
 const fileName = "index.html";
 
-const sourceFilePath = `../src/${fileName}`;
-const publicFilePath = `../public/${fileName}`;
+copyFile(sourcePathFor(fileName), publicPathFor(fileName));
 
-const source = path.join(__dirname, sourceFilePath);
-const public = path.join(__dirname, publicFilePath);
-
-fileSystem.copyFileSync(source, public);
-
-console.log(`Copied ${fileName} from src to public`);
+console.log(`Copied ${fileName} from src/ to public/`);
