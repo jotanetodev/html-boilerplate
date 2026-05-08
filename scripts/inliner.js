06364947eb01590844655abce28b353852ff692a
José Dias Neto <jdneto@live.com>
2026-05-08 20:20:23 -0300

    Improve `inliner` logging

--- scripts/inliner.js
+++ scripts/inliner.js
@@ -1,6 +1,8 @@
 // Inline source files into tags of public/index.html.
 
-import { publicPathFor, readFile, readFileAsBase64, writeFile, HTMLDocument } from "./helper.js"
+import { pathFor, publicPathFor, readFile, readFileAsBase64, writeFile, HTMLDocument } from "./helper.js"
+
+const relativePath = (path) => path.replace(pathFor("."), "")
 
 function tagInliner(document, elementFilePaths, inlineTag) {
   const newElement = document.createElement(inlineTag)
@@ -11,15 +13,19 @@ function tagInliner(document, elementFilePaths, inlineTag) {
   newElement.innerHTML = content
   elementFilePaths[0]?.[0].replaceWith(newElement)
   elementFilePaths.slice(1).forEach(([element,]) => element.remove())
+
+  console.info(`Inlined ${elementFilePaths.map(([, path]) => relativePath(path)).join(", ")} into <${inlineTag}>`)
 }
 
 function attributeInliner(filePathAttribute, elementFilePaths) {
   const elementFileContents = elementFilePaths.map(([element, path]) =>
-    [element, readFileAsBase64(path)]
+    [element, path, readFileAsBase64(path)]
   ).filter(([, fileContent]) => fileContent)
 
-  elementFileContents.forEach(([element, content]) => {
+  elementFileContents.forEach(([element, path, content]) => {
     element.setAttribute(filePathAttribute, content)
+
+    console.info(`Inlined ${relativePath(path)} into <${element.nodeName.toLowerCase()}> attribute ${filePathAttribute}`)
   })
 }
 
@@ -28,13 +34,11 @@ function inline(htmlPath, selector, filePathAttribute, { inlineTag = null } = {}
   const elementFilePaths = document.querySelectorAll(selector).map((element) =>
     [element, publicPathFor(element.getAttribute(filePathAttribute))]
   )
-  
+
   if (inlineTag) tagInliner(document, elementFilePaths, inlineTag)
   else attributeInliner(filePathAttribute, elementFilePaths)
 
   writeFile(htmlPath, document.toString())
-
-  console.log(`Inlined "${selector}" into ${htmlPath}`)
 }
 
 inline(publicPathFor("index.html"), "head > link[rel=stylesheet]", "href", { inlineTag: "style" })
